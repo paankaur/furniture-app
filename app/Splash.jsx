a8d9ac1cbed1c2c397bef2574e5efa68a1470e79
Paan <paan.kaur@gmail.com>
2025-09-26 15:07:32 +0300

    Create Splash screen

---
+++ app/Splash.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Text, Image, View, StyleSheet, Pressable } from "react-native";
+import Button from "@/components/Button";
+
+export default function Splash() {
+  return (
+    <View style={styles.container}>
+      <Image
+        resizeMode="contain"
+        source={require("../assets/images/splash.png")}
+        style={styles.image}
+      />
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>You'll Find</Text>
+        <Text style={styles.innerTitle}>All you need</Text>
+        <Text style={styles.title}>Here!</Text>
+      </View>
+      <Button
+        title="Sign Up"
+        onPress={() => {
+          console.log("Sign Up Pressed");
+        }}
+      />
+      <Pressable  onPress={() => console.log("Sign In Pressed")}>
+        <Text style={styles.footerText}>Sign In</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    /* margin: "auto", */
+    width: "100%",
+    height: "100%",
+    padding: 24,
+    borderWidth: 1,
+    
+  },
+  titleContainer: {
+    marginVertical: 54,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontFamily: "DM Sans",
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#303030",
+  },
+  innerTitle: {
+    fontFamily: "DM Sans",
+    fontSize: 40,
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fca34d",
+  },
+  footerText: {
+    fontFamily: "Montserrat",
+    fontSize: 16,
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    textAlign: "center",
+    color: "#4f63ac",
+    
+  },
+});
